7fb77ea445e468e374cf17d8774a810b3103a2e8
abhibksc <yuviabhishek269@gmail.com>
2024-01-16 18:57:12 +0530

    Iteration(for Loop)

---
+++ Iterations.js
@@ -0,0 +1,17 @@
+console.log("About FoR LooP");
+
+// In Java, a for loop is used to iterate over a range of values or elements. The general syntax of a for loop in Java is as follows:
+
+for (initialization; condition; update) {
+    // code to be executed
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+{
+    // Ther is a break and continue Keyword which helps to many condition... 
+}
